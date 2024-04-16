c1ecf9b06fc04cbf2c0df2fa6c54da12d5d2db15
wong2 <wonderfuly@gmail.com>
2024-04-16 21:27:36 +0800

    Add link to shortcut config

--- packages/extension/entrypoints/options/main.tsx
+++ packages/extension/entrypoints/options/main.tsx
@@ -1,11 +1,21 @@
+import { GlobalStyles } from '@mui/joy'
+import { CssVarsProvider } from '@mui/joy/styles'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CssVarsProvider } from '@mui/joy/styles'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CssVarsProvider defaultMode="system">
+      <GlobalStyles
+        styles={{
+          svg: {
+            color: 'var(--Icon-color)',
+            margin: 'var(--Icon-margin)',
+            fontSize: 'var(--Icon-fontSize, 24px)',
+          },
+        }}
+      />
       <div className="p-5 min-h-screen">
         <App />
       </div>
